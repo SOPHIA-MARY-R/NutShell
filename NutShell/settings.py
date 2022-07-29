31e54efe588a40092bf3dcef30dcd45d7e69b39a
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2022-07-29 10:20:21 +0530

    Updated Settings.py

--- NutShell/settings.py
+++ NutShell/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-i-g_3st4l(51c(pe6+kerslz_i4%fc-#fp7(=6qk8k=ffs#157'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
